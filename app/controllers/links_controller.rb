55f1db57ff8ce685ea07e25f21c454c18bfdd520
Marcel Sands <marcel.sands84@gmail.com>
2016-04-05 18:36:08 -0400

    update links controller and remove unecessary actions

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
-  # GET /links
-  # GET /links.json
   def index
     @links = Link.all
   end
@@ -10,15 +8,15 @@ class LinksController < ApplicationController
   # GET /links/1
   # GET /links/1.json
   def show
-  end
-
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit
+    if params[:slug]
+      @link = Link.find_by(slug: params[:slug])
+      if redirect_to @link.given_url
+        @link.clicks += 1
+        @link.save
+      end
+    else
+      @link = Link.find(params[:id])
+    end
   end
 
   # POST /links
@@ -37,30 +35,6 @@ class LinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
-  def destroy
-    @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_link
@@ -69,6 +43,6 @@ class LinksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def link_params
-    params.require(:link).permit(:given_url, :slug, :clicks, :snapshot, :title)
+    params.require(:link).permit(:given_url)
   end
 end
