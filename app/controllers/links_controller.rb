3970ddfa6dc48d72119c3206507fc19fd2539955
Marcel Sands <marcel.sands84@gmail.com>
2016-04-05 13:44:17 -0400

    add comments

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -62,6 +62,12 @@ class LinksController < ApplicationController
   end
 
   private
+  # Display entire shortened URL
+  def display_slug
+    ENV['BASE_URL'] + self.slug
+  end
+
+  # Create link slug after base URL
   def generate_slug
     self.slug = self.id.to_s(36)
     self.save
