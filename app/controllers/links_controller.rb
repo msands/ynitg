60ff6d00761133ffa47f8defe374cfc9d7ab7306
Marcel Sands <marcel.sands84@gmail.com>
2016-04-06 00:17:36 -0400

    match and verify code against example

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,12 +1,10 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show]
 
-  def index
-    @links = Link.all
-  end
+  #def index
+  #  @links = Link.all
+  #end
 
-  # GET /links/1
-  # GET /links/1.json
   def show
     if params[:slug]
       @link = Link.find_by(slug: params[:slug])
@@ -19,29 +17,27 @@ class LinksController < ApplicationController
     end
   end
 
-  # POST /links
-  # POST /links.json
   def create
     @link = Link.new(link_params)
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
+        format.html { redirect_to root_path, notice: 'Link was successfully created.' }
+        format.js
+        format.json { render action: 'show', status: :created, location: @link }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_link
-    @link = Link.find(params[:id])
+    @link = Link.find_by(slug: params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def link_params
     params.require(:link).permit(:given_url)
   end
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,15 +2,15 @@ class Link < ActiveRecord::Base
   mount_uploader :snapshot, SnapshotUploader
   after_create :generate_slug, :screenshot_scrape
 
-  # Display entire shortened URL
-  def display_slug
-    ENV['BASE_URL'] + self.slug
-  end
-
   # Create link slug after base URL
   def generate_slug
     self.slug = self.id.to_s(36)
     self.save
+  end
+
+  # Display entire shortened URL
+  def display_slug
+    ENV['BASE_URL'] + self.slug
   end
 
   # Get screenshot and title page
