fcbcc1c9c4072d1af348bd991965f62da70b53bb
Marcel Sands <marcel.sands84@gmail.com>
2016-04-06 17:19:04 -0400

    update screenshot worker typo

--- app/workers/screenshot.rb
+++ app/workers/screenshot.rb
@@ -3,7 +3,7 @@ class Screenshot
 
   def perform(link_id)
     link = Link.find(link_id)
-    file = Tempfile.new(["template_#{link.id.to_s}", '.jpg'], 'tmp', :encoding => 'ascii-bit')
+    file = Tempfile.new(["template_#{link.id.to_s}", '.jpg'], 'tmp', :encoding => 'ascii-8bit')
     file.write(IMGKit.new(link.given_url, quality: 50, width: 600).to_jpg)
     file.flush
     link.snapshot = file
